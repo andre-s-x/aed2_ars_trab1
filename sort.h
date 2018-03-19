195ef03ed6ac32e0ce5e7c3a53f3fab8903c76a8
asx <asx@icomp.ufam.edu.br>
2018-03-19 11:03:51 -0400

    Tentativa_1.h

---
+++ sort.h
@@ -0,0 +1,25 @@
+// swap
+
+void swap(int *xp, int *yp)
+
+// nobody yes door (traduz ao foot da letra)
+
+void bubbleSort(int arr[], int n)
+
+void selectionSort(int arr[], int n)
+
+void insertionSort(int arr[], int n)
+
+// quick
+
+int partition (int arr[], int low, int high)
+
+void quickSort(int arr[], int low, int high)
+
+// radix
+
+int getMax(int arr[], int n)
+
+void countSort(int arr[], int n, int exp)
+
+void radixsort(int arr[], int n)
